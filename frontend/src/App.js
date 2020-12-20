85716f00c5341c39d566eaf3bf90c393d47e6c75
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-20 22:47:38 +0100

    Import: imported react and react-router-dom Import: Header component Add: added Header component to App component with a Route to '/' page as home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Header from "./Component/Header";
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header>Hello there</header>
-    </div>
+      <Router>
+          <div className="App">
+            <Header />
+            <div className="container">
+                <Route exact path="/">Home page</Route>
+            </div>
+          </div>
+      </Router>
   );
 }
 
