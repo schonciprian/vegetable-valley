4d01e89809668d34eb8342abe79114f2c3db39b7
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-21 21:48:26 +0100

    Route: added grow-guide, my-jobs, weather-forecast endpoints. Import: Navbar component imported and used in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "./component/Header";
+import Navbar from "./component/Navbar";
 
 import './stylesheet/App.css';
 
@@ -11,8 +12,12 @@ function App() {
       <Router>
           <div className="App">
             <Header />
+            <Navbar />
             <div className="container">
                 <Route exact path="/">Home page</Route>
+                <Route exact path="/grow-guides">There are some infos about vegetable growing...</Route>
+                <Route exact path="/my-jobs">There are my jobs to do separated for every month</Route>
+                <Route exact path="/weather-forecast">Weather forecast api where you can select your city</Route>
             </div>
           </div>
       </Router>
