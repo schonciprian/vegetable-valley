b4f6a9adbe98984877b196b258f39e9cdd014b71
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-24 15:15:03 +0100

    Import: weather component Route: added weather component to it's endpoint instead of text on the weather page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "./component/Header";
 import Navbar from "./component/Navbar";
+import Weather from "./component/weather/Weather";
+
 
 import './stylesheet/App.css';
 
@@ -17,7 +19,7 @@ function App() {
                 <Route exact path="/">Home page</Route>
                 <Route exact path="/grow-guides">There are some infos about vegetable growing...</Route>
                 <Route exact path="/my-jobs">There are my jobs to do separated for every month</Route>
-                <Route exact path="/weather-forecast">Weather forecast api where you can select your city</Route>
+                <Route exact path="/weather-forecast" component={Weather} />
             </div>
           </div>
       </Router>
