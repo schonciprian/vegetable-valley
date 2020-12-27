02b30919eb4f10a1b1aab3868f891bf2b47a0dcb
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-27 23:22:56 +0100

    Bugfix: Fixing longitude and latitude problem. If coordinates were use together in a variable the dailyForecast API call got the information from the previous coordinates.

--- frontend/src/component/weather/Weather.js
+++ frontend/src/component/weather/Weather.js
@@ -7,15 +7,22 @@ export default function Weather() {
     const [weather, setWeather] = useState([]);
     const [weatherForecast, setWeatherForecast] = useState([]);
     const [city, setCity] = useState("Gyor");
-    let [coordinate, setCoordinate] = useState({"lon":17.64,"lat":47.68});
+    // QUESTION: if I want to set the coordinates in the first API call and the second wants to use it
+    // it will use the previous coordinates despite of it knows the correct coordinates.
+    // For make working it well, I need to set longitude and latitude separately, but why?
+    // const [coordinate, setCoordinate] = useState({"lon":17.64,"lat":47.68});
+    let latitude;
+    let longitude;
 
 
     useEffect(() => axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f6c317d5027246f70ca2f9fcbc4ea46c`)
         .then(response => {
+            // setCoordinate(response.data.coord)
             setWeather(response.data);
-            setCoordinate(response.data.coord)
+            latitude = response.data.coord.lat;
+            longitude = response.data.coord.lon;
         })
-        .then(() => axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinate.lat}&lon=${coordinate.lon}&units=metric&exclude=minutely,hourly&appid=f6c317d5027246f70ca2f9fcbc4ea46c`))
+        .then(() => axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude=minutely,hourly&appid=f6c317d5027246f70ca2f9fcbc4ea46c`))
         .then(response => {
             setWeatherForecast(response.data);
         }), [city]);
