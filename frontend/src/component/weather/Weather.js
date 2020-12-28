49cad18a23367fb1e026dc48d5748def577dbaf4
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-28 19:58:34 +0100

    Refactor: Removed unnecessary variables, checking if weather data not empty before return part of the hook, terminate repetition on calculating sunrise and sunset time. Add: Added new elements to next day weather container.

--- frontend/src/component/weather/Weather.js
+++ frontend/src/component/weather/Weather.js
@@ -14,7 +14,6 @@ export default function Weather() {
     let latitude;
     let longitude;
 
-
     useEffect(() => axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f6c317d5027246f70ca2f9fcbc4ea46c`)
         .then(response => {
             // setCoordinate(response.data.coord)
@@ -27,31 +26,26 @@ export default function Weather() {
             setWeatherForecast(response.data);
         }), [city]);
 
-    if (weather.length === 0) {
-        return <div>Loading...</div>;
-    }
-
-    let {temp, feels_like, temp_min, temp_max, pressure, humidity} = weather.main;
+    let {temp, humidity} = weather.main;
     temp = (temp-273.15).toFixed(1);
-    // feels_like = (feels_like-273.15).toFixed(1);
-    // temp_min = (temp_min-273.15).toFixed(1);
-    // temp_max = (temp_max-273.15).toFixed(1);
-
     const todayWindSpeed = ((weather.wind.speed)*3.6).toFixed(0);
     const rain = (weather.rain !== undefined ? weather.rain : {"1h":0, "3h":0});
-    console.log(rain["1h"]);
-
-    const {main, /*description*/} = weather.weather[0];
-    const weatherType = main;
-    // const weatherTypeDescription = description;
-
+    const weatherType = weather.weather[0].main;
     const cityName = weather.name;
     const country = weather.sys.country;
 
-    let {timezone_offset, daily} = weatherForecast;
+    // Creating a new object from weatherForecast API response with necessary information about the weather
+    const {timezone_offset, daily} = weatherForecast;
     let dailyForecast = [];
-
     let count = 0;
+
+    const calculateSunriseSunset = (dateTime) => {
+        return (dateTime.getHours() < 10 ? '0' + dateTime.getHours() : dateTime.getHours()) +
+            ':' +
+            (dateTime.getMinutes() < 10 ? '0' + dateTime.getMinutes() : dateTime.getMinutes())  +
+            ':' +
+            (dateTime.getSeconds() < 10 ? '0' + dateTime.getSeconds() : dateTime.getSeconds())
+    }
 
     if (weatherForecast.length !== 0 ) {daily.forEach( oneDay => {
         if (count > 0 && count < 5) {
@@ -62,22 +56,15 @@ export default function Weather() {
                 month: sunrise.getMonth() + 1,
                 date: sunrise.getDate(),
                 day: sunrise.getDay(),
-                sunrise: (sunrise.getHours() < 10 ? '0' + sunrise.getHours() : sunrise.getHours()) +
-                    ':' +
-                    (sunrise.getMinutes() < 10 ? '0' + sunrise.getMinutes() : sunrise.getMinutes())  +
-                    ':' +
-                    (sunrise.getSeconds() < 10 ? '0' + sunrise.getSeconds() : sunrise.getSeconds()),
-                sunset: (sunset.getHours() < 10 ? '0' + sunset.getHours() : sunset.getHours()) +
-                    ':' +
-                    (sunset.getMinutes() < 10 ? '0' + sunset.getMinutes() : sunset.getMinutes()) +
-                    ':' +
-                    (sunset.getSeconds() < 10 ? '0' + sunset.getSeconds() : sunset.getSeconds()),
+                sunrise: calculateSunriseSunset(sunrise),
+                sunset: calculateSunriseSunset(sunset),
                 dailyTemp: (oneDay.temp.day).toFixed(1),
             })
         }
         count += 1;
     })}
 
+    // Get the name of the day, 0 is Sunday ...
     const getDay = (day) => {
         switch(day) {
         case 0:
@@ -99,6 +86,9 @@ export default function Weather() {
         }
     }
 
+    if (weather.length === 0) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div>
@@ -148,16 +138,30 @@ export default function Weather() {
                     <div className="today-info-container">
                         <div className="today-info">
                             <div className="precipitation">
-                                <span className="title">PRECIPITATION</span>
-                                <span className="value">{rain["1h"]} mm</span>
+                                <span className="title">DAY</span>
+                                <span className="value">{rain["1h"]} °C</span>
                             </div>
                             <div className="humidity">
-                                <span className="title">HUMIDITY</span>
-                                <span className="value">34 %</span>
+                                <span className="title">NIGHT</span>
+                                <span className="value">34 °C</span>
                             </div>
                             <div className="wind">
                                 <span className="title">WIND</span>
                                 <span className="value">{todayWindSpeed} km/h</span>
+                            </div>
+                        </div>
+                        <div className="today-info">
+                            <div className="precipitation">
+                                <span className="title">PRECIPITATION</span>
+                                <span className="value">{rain["1h"]} %</span>
+                            </div>
+                            <div className="wind">
+                                <span className="title">RAIN</span>
+                                <span className="value">{todayWindSpeed} mm</span>
+                            </div>
+                            <div className="humidity">
+                                <span className="title">HUMIDITY</span>
+                                <span className="value">34 %</span>
                             </div>
                         </div>
                     </div>
