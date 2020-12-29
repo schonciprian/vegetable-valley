6041acab2185100629c246325c31da87dd31fb2d
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-29 01:41:31 +0100

    Implement: getSnowDailyForecast() method added, getDay() method added.

--- frontend/src/component/weather/DailyForecastGetterFunctions.js
+++ frontend/src/component/weather/DailyForecastGetterFunctions.js
@@ -22,6 +22,12 @@ export function getRainDailyForecast(dailyForecast, indexOfDailyForecast) {
         "";
 }
 
+export function getSnowDailyForecast(dailyForecast, indexOfDailyForecast) {
+    return (dailyForecast.length !== 0) ?
+        dailyForecast[indexOfDailyForecast].snow :
+        "";
+}
+
 export function getWindDailyForecast(dailyForecast, indexOfDailyForecast) {
     return (dailyForecast.length !== 0) ?
         dailyForecast[indexOfDailyForecast].wind :
@@ -32,4 +38,25 @@ export function getHumidityDailyForecast(dailyForecast, indexOfDailyForecast) {
     return (dailyForecast.length !== 0) ?
         dailyForecast[indexOfDailyForecast].humidity :
         "";
+}
+
+export function getDay(day) {
+    switch(day) {
+        case 0:
+            return 'Sunday';
+        case 1:
+            return 'Monday';
+        case 2:
+            return 'Tuesday';
+        case 3:
+            return 'Wednesday';
+        case 4:
+            return 'Thursday';
+        case 5:
+            return 'Friday';
+        case 6:
+            return 'Saturday';
+        default:
+            return 'Sunday';
+    }
 }
