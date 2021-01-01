79a1ce6e6728052d8f6af6d8d0441ccd4efcc5a8
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-01-01 23:48:30 +0100

    Modification: city-input field onChange changed to onKeyDown and onClick select text action added. city-input-container and it's elements styled.

--- frontend/src/component/weather/Weather.js
+++ frontend/src/component/weather/Weather.js
@@ -18,6 +18,16 @@ export default function Weather() {
                 })
         }, [city]);
 
+    const handleFocus = (event) => event.target.select();
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            setCity(event.target.value)
+            document.getElementById("city-selector-container").style.display = "none";
+            document.getElementById("city-selector").style.display = "none";
+        }
+    }
+
     if (weather.length === 0) {
         return <div className="loading">Loading data from server...</div>;
     } else {
@@ -33,7 +43,12 @@ export default function Weather() {
                      }}/>
 
                 <div id="city-selector" className="city-selector" style={{display: "none"}}>
-                    <input className="city-input" value={city} onChange={(e) => setCity(e.target.value)}/>
+                    <div className="city-input-container">
+                        <div className="city-input-text">Which city are you curious about?</div>
+                        <input className="city-input"
+                               onKeyDown={handleKeyDown}
+                               onClick={handleFocus}/>
+                    </div>
                     <div className="city-list-container">
                         <div className="city-list-hungary">
                             <div className="city-list-title">Chief towns of counties</div>
