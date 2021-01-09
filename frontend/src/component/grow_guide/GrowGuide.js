cad1eb786efa43b7d14b096d1fb1f40bbb97ccc3
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-01-10 00:22:54 +0100

    Implement: Started to implement flipping card effect to vegetable's card.

--- frontend/src/component/grow_guide/GrowGuide.js
+++ frontend/src/component/grow_guide/GrowGuide.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import {Vegetables} from './Descriptions';
 
+import garlic from '../../image/garlic.png';
+
+
 export default function GrowGuide() {
-    console.log(Vegetables);
+    // console.log(Vegetables);
+
+
+    const toggleCard = () => {
+        const card = document.querySelector('.card-inner');
+        console.log(card);
+        card.classList.toggle('is-flipped');
+    }
 
     return (
+        <div>
         <div className="grow-guides-container">
+
             {Object.keys(Vegetables).map((veggie, index) => (
-                <div className="grow-guide-card" key={index}>
-                    <img src={Vegetables[veggie].pictureURL} alt=""/>
-                    <div className="vegetable-name">{Vegetables[veggie].name}</div>
+                <div className="card" onClick={toggleCard}>
+                    <div className="grow-guide-card card-inner" key={index}>
+                        <div className="card-face card-face-front">
+                            <img src={Vegetables[veggie].pictureURL} alt=""/>
+                            <div className="vegetable-name">{Vegetables[veggie].name}</div>
+                        </div>
+                        <div className="card-face card-face-back">
+                            <div className="card-content">
+                                <div className="card-header">
+                                    <img className="pp" src={garlic} alt=""/>
+                                    <h2>Garlic</h2>
+                                </div>
+                                <div className="card-body">
+                                    <h3>Sowing depth: 3 cm</h3>
+                                    <p>some other things</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             ))}
+
+        </div>
+        {/*<div className="card" onClick={toggleCard}>*/}
+        {/*    <div id="card-inner" className="card-inner">*/}
+        {/*        <div className="card-face card-face-front">*/}
+        {/*            <h2>Garlic</h2>*/}
+        {/*        </div>*/}
+        {/*        <div className="card-face card-face-back">*/}
+        {/*            <div className="card-content">*/}
+        {/*                <div className="card-header">*/}
+        {/*                    <img className="pp" src={garlic} alt=""/>*/}
+        {/*                    <h2>Garlic</h2>*/}
+        {/*                </div>*/}
+        {/*                <div className="card-body">*/}
+        {/*                    <h3>Sowing depth: 3 cm</h3>*/}
+        {/*                    <p>some other things</p>*/}
+        {/*                </div>*/}
+        {/*            </div>*/}
+        {/*        </div>*/}
+        {/*    </div>*/}
+        {/*</div>*/}
         </div>
     );
 }
