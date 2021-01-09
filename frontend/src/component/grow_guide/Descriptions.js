cb342736763efd8e2734ee4a0a9e76561dc1c078
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-01-09 01:29:07 +0100

    Description object created with some vegetable's information.

---
+++ frontend/src/component/grow_guide/Descriptions.js
@@ -0,0 +1,51 @@
+import garlic from '../../image/garlic.png';
+import onion from '../../image/onion.png';
+import red_onion from '../../image/red_onion.png';
+import carrot from '../../image/carrot.png';
+import white_carrot from '../../image/white_carrot.png';
+
+
+
+export const Vegetables = {
+    garlic: {
+        name: 'Garlic',
+        pictureURL: garlic,
+        sow_depth: '1 cm',
+        spacing_between_rows: '30 cm',
+        spacing_along_row: '15 cm',
+        basic_information: 'Garlic is a perennial and is part of the onion family. ' +
+            'Garlic grows best in a full sun location in soils that are well-drained and high in organic matter. ' +
+            'Garlic is often mentioned as a superfood. ' +
+            'Some studies show that it may be effective against high blood pressure, cardiovascular disease, cholesterol, colds and some cancers. ' +
+            'Although these studies point to possible health benefits, you would have to consume a lot to get the desired effects. ' +
+            'That should not stop you growing and eating it. ' +
+            'Even without those benefits it adds an lovely strong flavour, which can replace salt in many dishes. ' +
+            'Even if the actual health effects are minimal Garlic is a great vegetable to add to your garden and diet.',
+        how_to_grow: 'Garlic grows best in a sunny fertile spot. Separate a head/bulb of garlic into individual cloves. ' +
+            'It is best to get garlic bulbs from a supplier that sells them for growing. ' +
+            'It is possible to use the regular garlic cloves that you buy in the shop, but there is a greater risk of disease. ' +
+            'Once separated plant the individual cloves about 5 cm deep (pointy end up) every 15 cm in rows 30 cm apart. ' +
+            'Avoid pushing the clove into the soil, instead make a hole with finger (or stick) and drop the clove in. ' +
+            'Keep the area free from weeds as yields will suffer if they are competing for light. ' +
+            'Also remove any flowering heads as they appear. ' +
+            'Garlic needs little watering, but yields will improve if watered during very dry periods. ' +
+            'Don\'t water formed bulbs as that can encourage rot.',
+        how_to_harvest: 'Once the leaves turn yellow in the summer, lift the bulbs and allow to dry under cover.',
+    },
+    onion: {
+        name: 'Onion',
+        pictureURL: onion,
+    },
+    red_onion: {
+        name: 'Red onion',
+        pictureURL: red_onion,
+    },
+    carrot: {
+        name: 'Carrot',
+        pictureURL: carrot,
+    },
+    white_carrot: {
+        name: 'White carrot',
+        pictureURL: white_carrot,
+    },
+}
