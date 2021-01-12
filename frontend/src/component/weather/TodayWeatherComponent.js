fd59e947fe6d3b5c88a27f8de101339e344ffb9b
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-01-12 22:57:33 +0100

    Comment: Comment added according to ask a mentor about it.

--- frontend/src/component/weather/TodayWeatherComponent.js
+++ frontend/src/component/weather/TodayWeatherComponent.js
@@ -19,6 +19,8 @@ export default function TodayWeatherComponent(props) {
         weatherIcon: weather.weather[0].icon,
     };
 
+    // This variable also occurs in WeatherForecastComponent.js. How can i put this to TodayWeatherFunctions.js
+    // to not duplicate it. It will also need todayWeather object in the other file.
     const getForecastIcon = (todayWeather.length !== 0) ?
         todayWeather.weatherIcon :
         '01d';
