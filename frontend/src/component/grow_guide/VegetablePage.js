fe1d2de0eff81453a51b5e5b93033945eb60cfa2
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-01-17 12:22:07 +0100

    Style: Started to create vegetablePage's structure with it's own style.

--- frontend/src/component/grow_guide/VegetablePage.js
+++ frontend/src/component/grow_guide/VegetablePage.js
@@ -7,13 +7,26 @@ function VegetablePage(props) {
     const vegetableInfo = Vegetables[vegetableName];
 
     return (
-        <div style={{color: "#eee"}}>
-            <Link style={{color: "#eee"}} to="/grow-guides">Back to grow guides</Link><br/><br/>
-            <div>{vegetableInfo.name}</div><br/>
-            <div>{vegetableInfo.basic_information}</div><br/>
-            <div>{vegetableInfo.how_to_grow}</div><br/>
-            <div>{vegetableInfo.how_to_harvest}</div><br/>
-            <img src={vegetableInfo.pictureURL} alt=""/>
+        <div className="vegetable-info-container">
+            <Link className="back-button" to="/grow-guides">Back to grow guides</Link><br/>
+            <div className="vegetable-info">
+                <div className="smaller-information">
+                    <div>{vegetableInfo.name}</div><br/>
+
+                    <img src={vegetableInfo.pictureURL} alt=""/>
+                    <div>{vegetableInfo.sow_depth}</div><br/>
+                    <div>{vegetableInfo.spacing_between_rows}</div><br/>
+                    <div>{vegetableInfo.spacing_along_row}</div><br/>
+                </div>
+                <div className="longer-information">
+                    <div>Basic information: <br/><br/>{vegetableInfo.basic_information}</div><br/>
+                    <div>How to grow: <br/><br/>{vegetableInfo.how_to_grow}</div><br/>
+                    <div>How to harvest: <br/><br/>{vegetableInfo.how_to_harvest}</div><br/>
+                </div>
+
+            </div>
+
+
         </div>
     );
 }
