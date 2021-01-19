10b2c38da018b608bc45c283604ad36b7d95e589
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-01-19 20:46:14 +0100

    Modification: Style modification on basic information about the vegetable.

--- frontend/src/component/grow_guides/vegetable/VegetablePage.js
+++ frontend/src/component/grow_guides/vegetable/VegetablePage.js
@@ -15,17 +15,17 @@ function VegetablePage(props) {
             <div className="vegetable-info">
                 <div className="smaller-information">
                     <div className="basic-information">
-                        <div>{vegetableInfo.name}</div>
+                        <div className="vegetable-name">{vegetableInfo.name}</div>
                         <img src={vegetableInfo.pictureURL} alt=""/>
-                        <div>
+                        <div className="vegetable-small-info">
                             <span>Sowing depth: </span>
                             <span>{vegetableInfo.sow_depth}</span>
                         </div>
-                        <div>
+                        <div className="vegetable-small-info">
                             <span>Line spacing: </span>
                             <span>{vegetableInfo.spacing_between_rows}</span>
                         </div>
-                        <div>
+                        <div className="vegetable-small-info">
                             <span>Inline spacing: </span>
                             <span>{vegetableInfo.spacing_along_row}</span>
                         </div>
