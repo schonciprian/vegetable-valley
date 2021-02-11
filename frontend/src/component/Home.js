fbe67870631b97296a56e31c6ae730e243147b83
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-02-11 18:44:34 +0100

    Add: Added more information about the application and a picture of my garden in 2020. Refactor: Creating li tags was refactored with a map function to avoid repetition.

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import {FaLeaf} from "react-icons/fa";
+import my_garden from '../my_garden.jpg';
 
 
 function Home() {
+
+    const vegetableInformation = [
+        "Sowing depth",
+        "Line spacing",
+        "Row spacing",
+        "Planting guidelines by month to month",
+        "How to grow",
+        "How to harvest",
+        "I love P",
+    ]
+
     return (
         <div className="home-container">
             <h1>Welcome to Vegetable Valley!</h1>
+
             <p className="self-introduction">My name is Ciprian and I love spending time in the garden while
                 I am listening to the chirping of birds or the noise of the wind.
                 I started gardening back in my childhood during summer breaks in my
-                grandparents garden with an own small plot.</p>
+                grandparents' garden with my own small plot.</p>
+
             <div className="app-introduction">
                 <h2>What is Vegetable Valley?</h2>
-                <p>Vegetable Valley is an online application where you can find many information about vegetables in the following topics:</p>
+                <p>Vegetable Valley is an online application where you can find many
+                    <span className="highlight"> information about vegetables </span>
+                    such as:</p>
                 <ul>
-                    <li><FaLeaf className="leaf-icon"/><span>Sowing depth</span></li>
-                    <li><FaLeaf className="leaf-icon"/><span>Line spacing</span></li>
-                    <li><FaLeaf className="leaf-icon"/><span>Row spacing</span></li>
-                    <li><FaLeaf className="leaf-icon"/><span>Planting guidelines by month to month</span></li>
-                    <li><FaLeaf className="leaf-icon"/><span>How to grow</span></li>
-                    <li><FaLeaf className="leaf-icon"/><span>How to harvest</span></li>
+                    {vegetableInformation.map((example, index) =>
+                        <li><FaLeaf className="leaf-icon"/><span>{vegetableInformation[index]}</span></li>)
+                    }
                 </ul>
+                <p>Vegetable Valley also helps you to search for
+                    <span className="highlight"> weather forecast </span>
+                    according to a chosen settlement. Knowing the following weather conditions would help a
+                    lot for you to plan your tasks in the garden for the next days.</p>
+            </div>
+
+            <div className="my-garden">
+                <h2>A picture of my small vegetable garden in 2020:</h2>
+                <img src={my_garden} alt="A picture of my garden." title="My garden plot in 2020" width="80%"/>
             </div>
 
         </div>
