bd4648cd11713e7c99d37d68588b074a065fc982
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-02-13 19:56:15 +0100

    Implement: Fullscreen option on clicking the picture of the garden. Import: FaIcons were imported to prepare "Contact me" page.

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import {FaLeaf} from "react-icons/fa";
+import React, {useState} from 'react';
+import {FaLeaf, FaGithub, FaLinkedin} from "react-icons/fa";
+import {GoPerson} from "react-icons/go";
+import {GrMail} from "react-icons/gr";
 import my_garden_1 from '../my_garden_1.jpg';
 import profile_picture from '../profile_picture.jpg';
 import {Link} from "react-router-dom";
 
 
 function Home() {
+
+    const [showFullScreen, setShowFullScreen] = useState(false);
+    const [openYAxis, setOpenYAxis] = useState(0);
 
     const vegetableInformation = [
         "Basic information",
@@ -16,6 +21,19 @@ function Home() {
         "How to grow",
         "How to harvest",
     ]
+
+    const fullScreen = () => {
+        setShowFullScreen(!showFullScreen);
+        console.log(showFullScreen);
+        setOpenYAxis(window.scrollY);
+    }
+
+    window.onscroll = () => {
+        if (window.scrollY < openYAxis - 150 || window.scrollY > openYAxis + 150) {
+            setShowFullScreen(false)
+        }
+    }
+
 
     return (
         <div className="home-container">
@@ -44,7 +62,14 @@ function Home() {
 
             <div className="my-garden">
                 <h2>A picture of my small vegetable garden in 2020:</h2>
-                <img src={my_garden_1} alt="A picture of my garden." title="My garden plot in 2020" width="80%"/>
+                <img onClick={fullScreen}
+                     style={showFullScreen
+                         ? {transform: "scale(1.2)", transition: "all 0.3s ease-out", margin: "70px 0"}
+                         : {transform: "scale(1)", transition: "all 0.3s ease-out"}}
+                     src={my_garden_1}
+                     alt="A picture of my garden."
+                     title="My garden plot in 2020"
+                     width="80%"/>
             </div>
 
         </div>
