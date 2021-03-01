90bd1c3d3940cdb24df9586eb9cd5a781fc94781
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-01 16:27:52 +0100

    Move: Contact component's style was moved to it's own stylesheet. Import: Contact component imports were reformatted, Contact stylesheet and comment lines were added.

--- frontend/src/component/basic/basic_sub/Contact.js
+++ frontend/src/component/basic/basic_sub/Contact.js
@@ -1,28 +1,43 @@
 import React from 'react';
+
+// Stylesheets
+import '../../../stylesheet/basic/basic_sub/Contact.css';
+
+// React-icons
 import {GoPerson} from "react-icons/go";
 import {GrMail} from "react-icons/gr";
 import {FaGithub, FaLinkedin} from "react-icons/fa";
+
+// Images
 import profile_picture from "../../../image/profile_picture/profile_picture.jpg";
+//**************************************************//
 
 export function Contact() {
     return (
         <div className="contact">
             <div className="personal-information">
                 <h2>Contact me:</h2>
-                <p><GoPerson className="contact-icon"/>&nbsp;<span>Name:</span>&nbsp;
-                    <span className="contact-value">Ciprián Schőn</span></p>
-                <p><GrMail className="contact-icon"/>&nbsp;<span>Email:</span>&nbsp;
-                    <a className="contact-value" href = "mailto: schon.ciprian@gmail.com" target="blank">
-                        schon.ciprian@gmail.com
-                    </a>
+                <p>
+                    <GoPerson className="contact-icon"/>&nbsp;
+                    <span>Name:</span>&nbsp;
+                    <span className="contact-value">Ciprián Schőn</span>
                 </p>
-                <p><FaGithub className="contact-icon"/>&nbsp;<a
-                    href="https://github.com/schonciprian/vegetable-valley" target="blank">Code on GitHub</a></p>
-                <p><FaLinkedin className="contact-icon"/>&nbsp;<a
-                    href="https://www.linkedin.com/in/cipri%C3%A1n-sch%C5%91n/" target="blank">LinkedIn profile</a>
+                <p>
+                    <GrMail className="contact-icon"/>&nbsp;
+                    <span>Email:</span>&nbsp;
+                    <a className="contact-value" href = "mailto: schon.ciprian@gmail.com" target="blank">
+                        schon.ciprian@gmail.com</a>
+                </p>
+                <p>
+                    <FaGithub className="contact-icon"/>&nbsp;
+                    <a href="https://github.com/schonciprian/vegetable-valley" target="blank">Code on GitHub</a>
+                </p>
+                <p>
+                    <FaLinkedin className="contact-icon"/>&nbsp;
+                    <a href="https://www.linkedin.com/in/cipri%C3%A1n-sch%C5%91n/" target="blank">LinkedIn profile</a>
                 </p>
             </div>
-            <img src={profile_picture} alt=""/>
+            <img src={profile_picture} alt="Picture of me"/>
         </div>
     );
 }
