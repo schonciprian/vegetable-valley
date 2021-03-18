1a2cc8737028c3d85fc5154cb11365d2a6cebcd8
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-18 15:43:58 +0100

    Fix: / was removed from BACKEND_DEPLOY_URL.

--- frontend/src/EnvironmentVariables.js
+++ frontend/src/EnvironmentVariables.js
@@ -1,4 +1,4 @@
 export const environmentVariables = {
     BACKEND_URL: "http://127.0.0.1:8000",
-    BACKEND_DEPLOY_URL: "https://powerful-caverns-53597.herokuapp.com/",
+    BACKEND_DEPLOY_URL: "https://powerful-caverns-53597.herokuapp.com",
 }
