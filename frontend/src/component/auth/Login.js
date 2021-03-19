7224be19a8fefd999b0717bfa141f2db48e02e43
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-19 11:02:22 +0100

    Context: Name property was added to SessionStorage and to User context.

--- frontend/src/component/auth/Login.js
+++ frontend/src/component/auth/Login.js
@@ -33,6 +33,7 @@ export default function Registration() {
             data: userData
         }).then((res) => {
             window.sessionStorage.setItem("token", res.data.token);
+            window.sessionStorage.setItem("name", res.data.name);
             swal("Successfully logged in", "Welcome back! You are redirected to the main page", "success");
             setTimeout(() => {
                 window.location.replace("/");
--- frontend/src/context/User.js
+++ frontend/src/context/User.js
@@ -3,7 +3,10 @@ import React, { useState, createContext } from "react";
 export const UserContext = createContext({});
 
 export const UserProvider = (props) => {
-    const [user, setUser] = useState({token: window.sessionStorage.getItem("token")});
+    const [user, setUser] = useState({
+        token: window.sessionStorage.getItem("token"),
+        name: window.sessionStorage.getItem("name"),
+    });
 
     return (
         <UserContext.Provider value={[user, setUser]}>
