76f9a01247d86570ca0a0901f95b87b366180663
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-19 16:53:35 +0100

    Implement: Logged in user data get from database and being able to modify it (saving to db not implemented).

--- frontend/src/component/basic/basic_main/Profile.js
+++ frontend/src/component/basic/basic_main/Profile.js
@@ -1,9 +1,42 @@
-import React, {useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import profile_picture from "../../../image/profile_picture/base_man.png";
 import '../../../stylesheet/basic/basic_main/Profile.css';
+import axios from "axios";
+import {environmentVariables} from "../../../EnvironmentVariables";
+import {UserContext} from "../../../context/User";
 
 function Profile(props) {
     const [editableFields, setEditableFields] = useState(false);
+    const [user] = useContext(UserContext);
+    const [userData, setUserData] = useState({});
+
+    useEffect(() => {
+        async function fetchData() {
+            await axios({
+                method: "get",
+                url: `${environmentVariables.BACKEND_URL}/api/get-user-data`,
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: "application/json, text/plain, */*",
+                    Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
+                },
+                data: {name: user["name"]}
+            }).then((res) => {
+                setUserData(res.data);
+            }).catch((error) => {
+                console.log(error.response.data);
+            })
+        }
+
+        fetchData();
+    }, [user])
+
+    const handleInputChange = (event, key) => {
+        setUserData(prevData => ({
+            ...prevData,
+            [key]: event.target.value
+        }))
+    }
 
     return (
         <div className="profile-page-container">
@@ -18,13 +51,21 @@ function Profile(props) {
                             <div className="profile-data-key">Name:</div>
                             <input className={`profile-data-value ${editableFields ? "editableField" : undefined}`}
                                    placeholder="Name"
-                                   readOnly={!editableFields}/>
+                                   value={userData.name}
+                                   readOnly={!editableFields}
+                                   onChange={(event) => {
+                                       handleInputChange(event, "name")
+                                   }}/>
                         </div>
                         <div className="profile-data-row">
                             <div className="profile-data-key">Email:</div>
                             <input className={`profile-data-value ${editableFields ? "editableField" : undefined}`}
                                    placeholder="Email"
-                                   readOnly={!editableFields}/>
+                                   value={userData.email}
+                                   readOnly={!editableFields}
+                                   onChange={(event) => {
+                                       handleInputChange(event, "email")
+                                   }}/>
                         </div>
                     </div>
 
