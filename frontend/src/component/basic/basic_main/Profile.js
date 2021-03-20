b30734c348cd0c627adc73efe7a92f4c3ed25d0d
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-20 23:33:51 +0100

    Add: Changing password tags added.

--- frontend/src/component/basic/basic_main/Profile.js
+++ frontend/src/component/basic/basic_main/Profile.js
@@ -122,6 +122,8 @@ function Profile(props) {
     //     return(year+'-' + month + '-'+dt);
     // }
 
+    console.log(userData);
+
     return (
         <div className="profile-page-container">
             <h1>Profile settings</h1>
@@ -165,11 +167,50 @@ function Profile(props) {
                 </div>
             </div>
 
-            <div className="delete-account">
-                <button onClick={deleteAccount}>
-                    Delete your account
-                </button>
+            <div className="profile-page-important">
+                <div className="profile-page-password">
+                    <div className="profile-data-row">
+                        <div className="profile-data-key">Old password:</div>
+                        <input className={`profile-data-value ${editableFields ? "editableField" : undefined}`}
+                               type="password"
+                               placeholder="Old password"
+                               onChange={(event) => {
+                                   console.log(event.target.value)
+                               }}/>
+                    </div>
+                    <div className="profile-data-row">
+                        <div className="profile-data-key">New password:</div>
+                        <input className={`profile-data-value ${editableFields ? "editableField" : undefined}`}
+                               type="password"
+                               placeholder="New password"
+                               onChange={(event) => {
+                                   console.log(event.target.value)
+                               }}/>
+                    </div>
+                    <div className="profile-data-row">
+                        <div className="profile-data-key">New password again:</div>
+                        <input className={`profile-data-value ${editableFields ? "editableField" : undefined}`}
+                               type="password"
+                               placeholder="New password again"
+                               onChange={(event) => {
+                                   console.log(event.target.value)
+                               }}/>
+                    </div>
+                    <button className="change-password-button" onClick={(event) => {
+                        console.log("save password")
+                    }}>
+                        Save
+                    </button>
+                </div>
+
+                <div className="delete-account">
+                    <h1>This action can not be reversed!</h1>
+                    <button onClick={deleteAccount}>
+                        Delete your account
+                    </button>
+                </div>
             </div>
+
         </div>
     );
 }
