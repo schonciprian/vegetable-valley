e0d853772802409da750fabc56d8bbc8b7251876
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-21 22:34:13 +0100

    Implement: Changing password call to backend.

--- frontend/src/component/basic/basic_main/profile/Password.js
+++ frontend/src/component/basic/basic_main/profile/Password.js
@@ -1,39 +1,73 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
+import {environmentVariables} from "../../../../EnvironmentVariables";
 
 function Password(props) {
+    const [passwordData, setPasswordData] = useState({});
+
+    const handleInputChange = (event, key) => {
+        setPasswordData(prevData => ({
+            ...prevData,
+            [key]: event.target.value
+        }))
+    }
+
+    const updateUserPassword = () => {
+        async function fetchData() {
+            await axios({
+                method: "put",
+                url: `${environmentVariables.BACKEND_URL}/api/update-user-password`,
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: "application/json, text/plain, */*",
+                    Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
+                },
+                data: passwordData
+            }).then((res) => {
+                console.log(res.data.message);
+                console.log("success")
+            }).catch((error) => {
+                console.log(error.response.data);
+            })
+        }
+
+        fetchData();
+    }
+
     return (
         <div className="profile-page-password">
             <div className="profile-data-row">
-                <div className="profile-data-key">Old password:</div>
-                <input className="profile-data-value"
+                <div className="profile-data-key">Current password:</div>
+                <input id="current_password"
+                       className="profile-data-value"
                        type="password"
-                       placeholder="Old password"
+                       placeholder="Current password"
                        onChange={(event) => {
-                           console.log(event.target.value)
+                           handleInputChange(event, "current_password")
                        }}/>
             </div>
             <div className="profile-data-row">
                 <div className="profile-data-key">New password:</div>
-                <input className="profile-data-value"
+                <input id="new_password"
+                       className="profile-data-value"
                        type="password"
                        placeholder="New password"
                        onChange={(event) => {
-                           console.log(event.target.value)
+                           handleInputChange(event, "new_password")
                        }}/>
             </div>
             <div className="profile-data-row">
                 <div className="profile-data-key">New password again:</div>
-                <input className="profile-data-value"
+                <input id="new_password_confirmation"
+                       className="profile-data-value"
                        type="password"
                        placeholder="New password again"
                        onChange={(event) => {
-                           console.log(event.target.value)
+                           handleInputChange(event, "new_password_confirmation")
                        }}/>
             </div>
 
-            <button className="change-password-button" onClick={(event) => {
-                console.log("save password")
-            }}>
+            <button className="change-password-button" onClick={updateUserPassword}>
                 Save
             </button>
         </div>
