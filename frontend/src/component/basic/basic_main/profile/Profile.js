f92d72cd2d188d6388cb0efd2a52f278b7c72d08
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-22 16:30:47 +0100

    Refactor: Remove async await calls, swal added when profile data changed.

--- frontend/src/component/basic/basic_main/profile/Profile.js
+++ frontend/src/component/basic/basic_main/profile/Profile.js
@@ -7,6 +7,7 @@ import {UserContext} from "../../../../context/User";
 import Password from "./Password";
 import ProfileData from "./ProfileData";
 import Delete from "./Delete";
+import Swal from "sweetalert2";
 
 function Profile(props) {
     const [editableFields, setEditableFields] = useState(false);
@@ -14,24 +15,20 @@ function Profile(props) {
     const [userData, setUserData] = useState({});
 
     useEffect(() => {
-        async function fetchData() {
-            await axios({
-                method: "get",
-                url: `${environmentVariables.BACKEND_URL}/api/get-user-data`,
-                headers: {
-                    'Content-Type': 'application/json',
-                    Accept: "application/json, text/plain, */*",
-                    Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
-                },
-                data: {username: user["username"]}
-            }).then((res) => {
-                setUserData(res.data);
-            }).catch((error) => {
-                console.log(error.response.data);
-            })
-        }
-
-        fetchData();
+        axios({
+            method: "get",
+            url: `${environmentVariables.BACKEND_URL}/api/get-user-data`,
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: "application/json, text/plain, */*",
+                Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
+            },
+            data: {username: user["username"]}
+        }).then((res) => {
+            setUserData(res.data);
+        }).catch((error) => {
+            console.log(error.response.data);
+        })
     }, [user])
 
     const handleInputChange = (event, key) => {
@@ -43,48 +40,36 @@ function Profile(props) {
 
     const updateUserData = () => {
         setEditableFields(!editableFields)
+        axios({
+            method: "put",
+            url: `${environmentVariables.BACKEND_URL}/api/update-user-data`,
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: "application/json, text/plain, */*",
+                Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
+            },
+            data: userData
+        }).then((res) => {
+            Swal.fire({
+                toast: true,
+                icon: 'success',
+                title: 'Personal data changed successfully',
+                animation: true,
+                position: 'top-right',
+                showConfirmButton: false,
+                timer: 3000,
+            });
 
-        async function fetchData() {
-            await axios({
-                method: "put",
-                url: `${environmentVariables.BACKEND_URL}/api/update-user-data`,
-                headers: {
-                    'Content-Type': 'application/json',
-                    Accept: "application/json, text/plain, */*",
-                    Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
-                },
-                data: userData
-            }).then((res) => {
-                console.log("success")
-            }).catch((error) => {
-                console.log(error.response.data);
-            })
-        }
+            setUser(prevData => ({
+                ...prevData,
+                username: userData.username
+            }));
 
-        fetchData();
-
-        setUser(prevData => ({
-            ...prevData,
-            username: userData.username
-        }))
-        window.sessionStorage.setItem("username", userData.username);
+            window.sessionStorage.setItem("username", userData.username);
+        }).catch((error) => {
+            console.log(error.response.data);
+        })
     }
-
-    // const createDate = () => {
-    //     let date = new Date(userData.created_at);
-    //     let year = date.getFullYear();
-    //     let month = date.getMonth()+1;
-    //     let dt = date.getDate();
-    //
-    //     if (dt < 10) {
-    //         dt = '0' + dt;
-    //     }
-    //     if (month < 10) {
-    //         month = '0' + month;
-    //     }
-    //
-    //     return(year+'-' + month + '-'+dt);
-    // }
 
     return (
         <div className="profile-page-container">
@@ -98,14 +83,15 @@ function Profile(props) {
                                  userData={userData}
                                  handleInputChange={handleInputChange}/>
 
-                    <button onClick={updateUserData}>
-                        {!editableFields ? "Edit" : "Save"}
-                    </button>
+                    {!editableFields
+                        ? <button onClick={() => setEditableFields(!editableFields)}>Edit</button>
+                        : <button onClick={updateUserData}>Save</button>
+                    }
                 </div>
             </div>
 
             <div className="profile-page-important">
-                <Password />
+                <Password/>
 
                 <Delete userData={userData}/>
             </div>
