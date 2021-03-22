280271866914822d79c57cdaf14cac7457784dc3
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-22 16:43:02 +0100

    Refactor: Remove async await.

--- frontend/src/component/basic/basic_main/profile/Password.js
+++ frontend/src/component/basic/basic_main/profile/Password.js
@@ -4,7 +4,7 @@ import {environmentVariables} from "../../../../EnvironmentVariables";
 import * as Swal from "sweetalert2";
 import {BiShow} from "react-icons/bi";
 
-function Password(props) {
+function Password() {
     const [passwordData, setPasswordData] = useState({});
     const [passwordShown, setPasswordShown] = useState({
         current_password: false,
@@ -29,36 +29,34 @@ function Password(props) {
     }
 
     const updateUserPassword = () => {
-        async function fetchData() {
-            await axios({
-                method: "put",
-                url: `${environmentVariables.BACKEND_URL}/api/update-user-password`,
-                headers: {
-                    'Content-Type': 'application/json',
-                    Accept: "application/json, text/plain, */*",
-                    Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
-                },
-                data: passwordData
-            }).then((res) => {
-                console.log("success")
-                document.getElementById("current_password").value = "";
-                document.getElementById("new_password").value = "";
-                document.getElementById("new_password_confirmation").value = "";
-                Swal.fire({
-                    toast: true,
-                    icon: 'success',
-                    title: 'Password changed successfully',
-                    animation: true,
-                    position: 'top-right',
-                    showConfirmButton: false,
-                    timer: 3000,
-                })
-            }).catch((error) => {
-                console.log(error.response.data);
-                setPasswordError(error.response.data)
+        axios({
+            method: "put",
+            url: `${environmentVariables.BACKEND_URL}/api/update-user-password`,
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: "application/json, text/plain, */*",
+                Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
+            },
+            data: passwordData
+        }).then((res) => {
+            document.getElementById("current_password").value = "";
+            document.getElementById("new_password").value = "";
+            document.getElementById("new_password_confirmation").value = "";
+
+            Swal.fire({
+                toast: true,
+                icon: 'success',
+                title: 'Password changed successfully',
+                animation: true,
+                position: 'top-right',
+                showConfirmButton: false,
+                timer: 3000,
             })
-        }
-        fetchData();
+
+        }).catch((error) => {
+            console.log(error.response.data);
+            setPasswordError(error.response.data)
+        })
     }
 
     return (
@@ -76,7 +74,7 @@ function Password(props) {
                            handleInputChange(event, "current_password")
                        }}/>
                 <BiShow className={`show-icon ${passwordShown.current_password ? "active" : undefined}`}
-                        onClick={(event) => handlePasswordShownChange(event,"current_password")}/>
+                        onClick={(event) => handlePasswordShownChange(event, "current_password")}/>
             </div>
 
             <div className="profile-data-row">
@@ -89,7 +87,7 @@ function Password(props) {
                            handleInputChange(event, "new_password")
                        }}/>
                 <BiShow className={`show-icon ${passwordShown.new_password ? "active" : undefined}`}
-                        onClick={(event) => handlePasswordShownChange(event,"new_password")}/>
+                        onClick={(event) => handlePasswordShownChange(event, "new_password")}/>
             </div>
 
             <div className="profile-data-row">
@@ -102,7 +100,7 @@ function Password(props) {
                            handleInputChange(event, "new_password_confirmation")
                        }}/>
                 <BiShow className={`show-icon ${passwordShown.new_password_confirmation ? "active" : undefined}`}
-                        onClick={(event) => handlePasswordShownChange(event,"new_password_confirmation")}/>
+                        onClick={(event) => handlePasswordShownChange(event, "new_password_confirmation")}/>
             </div>
 
             <button className="change-password-button" onClick={updateUserPassword}>
