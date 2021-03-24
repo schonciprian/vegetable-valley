d2074d1699b75fc2cee02a533ec637d9f19a87a1
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-24 16:34:49 +0100

    Add: PublicRoute component to use for redirects.

---
+++ frontend/src/component/redirects/PublicRoute.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom'
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    const isLoggedIn = window.sessionStorage.getItem("token");
+
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                !isLoggedIn
+                    ? (<Component {...props} />)
+                    : (<Redirect to={{ pathname: '/', state: { from: props.location } }} />)
+            }
+        />
+    )
+}
+
+export default PrivateRoute
