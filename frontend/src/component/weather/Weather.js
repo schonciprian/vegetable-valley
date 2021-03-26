7665418587b1028c378e7a8b91a78824e76e989e
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-26 08:36:17 +0100

    Add: Add more charts to the page.

--- frontend/src/component/weather/Weather.js
+++ frontend/src/component/weather/Weather.js
@@ -44,9 +44,9 @@ export default function Weather() {
                     <WeatherForecastComponent coordinate={coordinate}/>
                 </div>
                 <div className="charts">
-                    <Chart data={[12.4, 14.6, 9.5, 12.0, 15.9, 15.8, 15.9]} title={'Daily average temperature'}/>
-                    <Chart data={[15.4, 18.6, 10.5, 16.0, 13.9, 18.8, 19.9]} title={'Daily max temperature'}/>
-                    <Chart data={[10.4, 9.6, 6.5, 8.0, 13.9, 11.8, 12.9]} title={'Daily min temperature'}/>
+                    <Chart data={[12.4, 14.6, 9.5, 12.0, 15.9, 15.8, 15.9]} title={'Daily average temperature'} color={"#ff8c00"}/>
+                    <Chart data={[15.4, 18.6, 10.5, 16.0, 13.9, 18.8, 19.9]} title={'Daily max temperature'} color={"#d40505"}/>
+                    <Chart data={[10.4, 9.6, 6.5, 8.0, 13.9, 11.8, 12.9]} title={'Daily min temperature'} color={"#00adfc"}/>
                 </div>
             </div>
         );
