8166b07664c30941ac7b508a6d38e2f649522b02
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-26 14:19:14 +0100

    Remove: Charts were removed.

--- frontend/src/component/weather-forecast/weather/Weather.js
+++ frontend/src/component/weather-forecast/weather/Weather.js
@@ -7,9 +7,6 @@ import '../../../stylesheet/weather/Today_Weather.css';
 import '../../../stylesheet/weather/Forecast_Weather.css';
 import '../../../stylesheet/weather/City_Selector.css';
 import '../../../stylesheet/weather/Weather_forecast.css';
-import Chart from "../charts/Chart";
-
-
 
 export default function Weather() {
     const [weather, setWeather] = useState([]);
@@ -42,11 +39,6 @@ export default function Weather() {
                 <div className="weather-container">
                     <TodayWeatherComponent weather={weather}/>
                     <WeatherForecastComponent coordinate={coordinate}/>
-                </div>
-                <div className="charts">
-                    <Chart data={[12.4, 14.6, 9.5, 12.0, 15.9, 15.8, 15.9]} title={'Daily average temperature'} color={"#ff8c00"}/>
-                    <Chart data={[15.4, 18.6, 10.5, 16.0, 13.9, 18.8, 19.9]} title={'Daily max temperature'} color={"#d40505"}/>
-                    <Chart data={[10.4, 9.6, 6.5, 8.0, 13.9, 11.8, 12.9]} title={'Daily min temperature'} color={"#00adfc"}/>
                 </div>
             </div>
         );
