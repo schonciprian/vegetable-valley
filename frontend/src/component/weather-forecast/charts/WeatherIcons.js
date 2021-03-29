053442406d4b2b021b19b6f817c07eb85c9b42a5
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-29 16:13:53 +0200

    Add: WeatherIcons component, to show the weather for the user with icons. Not using the api data, just for testing purposes.

---
+++ frontend/src/component/weather-forecast/charts/WeatherIcons.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function WeatherIcons(props) {
+    return (
+        <div className="icons">
+            <img src="https://openweathermap.org/img/w/01d.png" alt=""/>
+            <img src="https://openweathermap.org/img/w/02d.png" alt=""/>
+            <img src="https://openweathermap.org/img/w/02d.png" alt=""/>
+            <img src="https://openweathermap.org/img/w/04d.png" alt=""/>
+            <img src="https://openweathermap.org/img/w/03d.png" alt=""/>
+            <img src="https://openweathermap.org/img/w/01d.png" alt=""/>
+            <img src="https://openweathermap.org/img/w/02d.png" alt=""/>
+            <img src="https://openweathermap.org/img/w/03d.png" alt=""/>
+        </div>
+    );
+}
+
+export default WeatherIcons;
