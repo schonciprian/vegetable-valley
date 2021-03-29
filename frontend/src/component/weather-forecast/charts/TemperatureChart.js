42987b208e7401a07c8fe8226dd7ca623b42f15b
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-29 16:14:55 +0200

    Add: WeatherIcons to every chart.

--- frontend/src/component/weather-forecast/charts/TemperatureChart.js
+++ frontend/src/component/weather-forecast/charts/TemperatureChart.js
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react';
 import Chart from "./Chart";
 import {WeatherForecastDataContext} from "../../../context/WeatherForecastDataContext";
+import WeatherIcons from "./WeatherIcons";
 
 function TemperatureChart() {
     const [weatherForecastData] = useContext(WeatherForecastDataContext);
@@ -8,9 +9,21 @@ function TemperatureChart() {
     return (
         weatherForecastData.avgTemp
             ? <div className="charts">
-                <Chart data={weatherForecastData.avgTemp} title={'Daily average temperature'} color={"#ff8c00"}/>
-                <Chart data={weatherForecastData.maxTemp} title={'Daily max temperature'} color={"#d40505"}/>
-                <Chart data={weatherForecastData.minTemp} title={'Daily min temperature'} color={"#00adfc"}/>
+                <div className="chart">
+                    <WeatherIcons />
+                    <Chart data={weatherForecastData.avgTemp} title={'Daily average temperature'} color={"#ff8c00"}/>
+                </div>
+
+                <div className="chart">
+                    <WeatherIcons />
+                    <Chart data={weatherForecastData.maxTemp} title={'Daily max temperature'} color={"#d40505"}/>
+                </div>
+
+                <div className="chart">
+                    <WeatherIcons />
+                    <Chart data={weatherForecastData.minTemp} title={'Daily min temperature'} color={"#00adfc"}/>
+                </div>
+
                 {/*<Chart data={weatherForecastData.wind} title={'Daily min temperature'} color={"#00adfc"}/>*/}
             </div>
             : <React.Fragment/>
