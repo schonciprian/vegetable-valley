687a8601e551f3a81ef477fbea27d9bf1eb1bdf1
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-31 16:35:54 +0200

    Add: RainChart component.

---
+++ frontend/src/component/weather-forecast/chart/chart_types/RainChart.js
@@ -0,0 +1,46 @@
+import React, {useContext} from 'react';
+
+// Data context
+import {WeatherForecastDataContext} from "../../../../context/WeatherForecastDataContext";
+
+// Chart related
+import ChartComponent from "../ChartComponent";
+import WeatherIcons from "../WeatherIcons";
+
+//**************************************************//
+
+function RainChart() {
+    const [weatherForecastData] = useContext(WeatherForecastDataContext);
+
+    const chartYAxisValues = () => {
+        const dataSpacing = 5; // Values on y axis will be divisible by this number. Eg.: 5 -> 25 20 15 10 5 0
+
+        let max = (Math.round(Math.max(...weatherForecastData.rain) / dataSpacing)) * dataSpacing + dataSpacing;
+        let min = 0;
+        let tickAmount = ((max - min) / dataSpacing);
+
+        return {
+            max: max,
+            min: min,
+            tickAmount: tickAmount,
+        };
+    }
+
+    return (
+        <div className="charts">
+            <div className="chart">
+                <WeatherIcons weatherIcons={weatherForecastData.weatherIcons}/>
+                <ChartComponent data={weatherForecastData.rain}
+                                seriesName={"Rain mm"}
+                                title={'Daily rain forecast'}
+                                color={"#667ae3"}
+                                chartYAxisValues={chartYAxisValues()}
+                                xAxisCategories={weatherForecastData.dayNames}
+                                unitOfMeasure={'mm'}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default RainChart;
