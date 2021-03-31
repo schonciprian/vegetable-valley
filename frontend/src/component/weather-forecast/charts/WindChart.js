18e391b2a30b973b52b6c8c3bc4966b4ee1371eb
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-31 12:02:10 +0200

    Add: WindChart component.

---
+++ frontend/src/component/weather-forecast/charts/WindChart.js
@@ -0,0 +1,29 @@
+import React, {useContext} from 'react';
+import WeatherIcons from "./WeatherIcons";
+import ChartComponent from "./ChartComponent";
+import {WeatherForecastDataContext} from "../../../context/WeatherForecastDataContext";
+
+function WindChart(props) {
+    const [weatherForecastData] = useContext(WeatherForecastDataContext);
+
+    console.log(weatherForecastData.wind);
+
+    return (
+        <div className="charts">
+            <div className="chart">
+                <WeatherIcons weatherIcons={weatherForecastData.weatherIcons}/>
+                <ChartComponent data={weatherForecastData.wind}
+                                title={'Daily average wind speed'}
+                                color={"#ff8c00"}
+                                chartYAxisValues={{
+                                    max: 30,
+                                    min: 0,
+                                    tickAmount: 6,
+                                }}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default WindChart;
