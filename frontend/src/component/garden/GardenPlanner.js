417c07ed5c2dab8d5aab98bd8dbdb4ab93cc745b
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-04-22 15:57:53 +0200

    Add missing useRef import.

--- frontend/src/component/garden/GardenPlanner.js
+++ frontend/src/component/garden/GardenPlanner.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import '../../stylesheet/garden/GardenPlanner.css'
 import {BsFillPlusCircleFill} from "react-icons/bs";
 import {GardenSizeContext} from "./GardenSizeContext";
