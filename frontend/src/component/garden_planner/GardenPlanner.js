3f4ea286e157d5f743da2575eb7c41d8054360cb
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-04-27 15:34:47 +0200

    Refactor: GardenSelection.js moved to it's own component.

--- frontend/src/component/garden_planner/GardenPlanner.js
+++ frontend/src/component/garden_planner/GardenPlanner.js
@@ -9,7 +9,7 @@ import Garden from "./garden_connected/Garden";
 import DownloadGarden from "./garden_connected/garden_settings/DownloadGarden";
 import axios from "axios";
 import {environmentVariables} from "../../EnvironmentVariables";
-import {FaArrowAltCircleLeft, FaArrowAltCircleRight} from "react-icons/fa";
+import GardenSelection from "./garden_connected/garden_settings/GardenSelection";
 
 function GardenPlanner() {
     const gardenRef = useRef(null);
@@ -18,10 +18,6 @@ function GardenPlanner() {
     const [draggedVegetable, setDraggedVegetable] = useState({})
     const [gardenSize, setGardenSize] = useContext(GardenSizeContext);
     const [selectedOptionList, setSelectedOptionList] = useState("Vegetables")
-    const [editableTitle, setEditableTitle] = useState(false)
-    const [gardenName, setGardenName] = useState("Your garden")
-    const [inputError, setInputError] = useState(false)
-    const [gardenTemporaryName, setGardenTemporaryName] = useState(gardenName)
 
     useEffect(() => {
         if (!window.sessionStorage.getItem("token")) return
@@ -43,26 +39,7 @@ function GardenPlanner() {
         }).catch((error) => {
             console.log(error.response.data)
         })
-
-        document.addEventListener('mousedown', handleClickOutside);
-
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-
-    }, [setGardenSize, gardenName])
-
-    const handleClickOutside = (e) => {
-        if (gardenTitleRef.current && gardenTitleRef.current.contains(e.target)) {
-            // inside click
-            setEditableTitle(true);
-            return;
-        }
-        // outside click
-        setEditableTitle(false);
-        setGardenTemporaryName(gardenName) // Change back temp value to original garden name
-        setInputError(false)
-    };
+    }, [setGardenSize])
 
     const saveSizeChangesToDatabase = (rows, columns) => {
         axios({
@@ -111,44 +88,11 @@ function GardenPlanner() {
         }
     }
 
-    const handleGardenNameChange = (event) => {
-        setGardenTemporaryName(event.target.value)
-    }
-
-    const handleKeyPress = (event) => {
-        const gardenNewName = event.target.value;
-
-        if (event.key === "Enter") {
-            if (gardenNewName.length > 0) {
-                setGardenName(gardenNewName)
-                setGardenTemporaryName(gardenNewName)
-                setEditableTitle(false)
-                setInputError(false)
-            } else {
-                setInputError(true)
-            }
-        }
-    }
-
     return (
         <div className="garden-planner">
             <div className="garden-container">
-                <div className='garden-selection'>
-                    <FaArrowAltCircleLeft className="arrow"/>
-                    <input
-                        className={`profile-data-value ${editableTitle ? "editableField" : ""} ${inputError ? "error" : ""}`}
-                        placeholder="Your garden's name"
-                        value={editableTitle ? gardenTemporaryName : gardenName}
-                        maxLength={18}
-                        ref={gardenTitleRef}
-                        readOnly={!editableTitle}
-                        onKeyDown={event => handleKeyPress(event)}
-                        onChange={(event) => {
-                            handleGardenNameChange(event)
-                        }}
-                    />
-                    <FaArrowAltCircleRight className="arrow"/>
-                </div>
+                <GardenSelection gardenTitleRef={gardenTitleRef}/>
+
                 <div className="option-selection">
 
                     <DownloadGarden gardenRef={gardenRef} rows={gardenSize.rows} columns={gardenSize.columns}/>
---
+++ frontend/src/component/garden_planner/garden_connected/garden_settings/GardenSelection.js
@@ -0,0 +1,69 @@
+import React, {useEffect, useState} from 'react';
+import {FaArrowAltCircleLeft, FaArrowAltCircleRight} from "react-icons/fa";
+
+function GardenSelection(props) {
+    const gardenTitleRef = props.gardenTitleRef;
+
+    const [editableTitle, setEditableTitle] = useState(false)
+    const [gardenName, setGardenName] = useState("Your garden")
+    const [inputError, setInputError] = useState(false)
+    const [gardenTemporaryName, setGardenTemporaryName] = useState(gardenName)
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [gardenName])
+
+    const handleClickOutside = (e) => {
+        if (gardenTitleRef.current && gardenTitleRef.current.contains(e.target)) {
+            // inside click
+            setEditableTitle(true);
+            return;
+        }
+        // outside click
+        setEditableTitle(false);
+        setGardenTemporaryName(gardenName) // Change back temp value to original garden name
+        setInputError(false)
+    };
+
+    const handleGardenNameChange = (event) => {
+        setGardenTemporaryName(event.target.value)
+    }
+
+    const handleKeyPress = (event) => {
+        const gardenNewName = event.target.value;
+
+        if (event.key === "Enter") {
+            if (gardenNewName.length > 0) {
+                setGardenName(gardenNewName)
+                setGardenTemporaryName(gardenNewName)
+                setEditableTitle(false)
+                setInputError(false)
+            } else {
+                setInputError(true)
+            }
+        }
+    }
+    return (
+        <div className='garden-selection'>
+            <FaArrowAltCircleLeft className="arrow"/>
+            <input
+                className={`profile-data-value ${editableTitle ? "editableField" : ""} ${inputError ? "error" : ""}`}
+                placeholder="Your garden's name"
+                value={editableTitle ? gardenTemporaryName : gardenName}
+                maxLength={18}
+                ref={gardenTitleRef}
+                readOnly={!editableTitle}
+                onKeyDown={event => handleKeyPress(event)}
+                onChange={(event) => {
+                    handleGardenNameChange(event)
+                }}
+            />
+            <FaArrowAltCircleRight className="arrow"/>
+        </div>);
+}
+
+export default GardenSelection;
