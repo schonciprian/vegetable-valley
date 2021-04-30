b0ecb7249273630936f4ef9d0a4173365a7ab0bc
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-04-30 15:47:13 +0200

    Refactor: GardenOptionContainer related methods and JSC outsourced to it's own component.

--- frontend/src/component/garden_planner/GardenPlanner.js
+++ frontend/src/component/garden_planner/GardenPlanner.js
@@ -1,17 +1,11 @@
-import React, {useContext, useEffect, useRef, useState} from 'react';
+import React, { useRef, useState} from 'react';
 import '../../stylesheet/garden/GardenPlanner.css'
-import {BsFillPlusCircleFill} from "react-icons/bs";
-import {GardenSizeContext} from "./garden_connected/garden_settings/GardenSizeContext";
 import OptionVegetableList from "./block_connected/OptionVegetableList";
 import OptionBlockList from "./block_connected/OptionBlockList";
 
 import Garden from "./garden_connected/Garden";
-import DownloadGarden from "./garden_connected/garden_settings/DownloadGarden";
-import axios from "axios";
-import {environmentVariables} from "../../EnvironmentVariables";
 import GardenSelection from "./garden_connected/GardenSelection";
-import {ActualGardenIdContext} from "./garden_connected/ActualGardenIdContext";
-import {getRequest, putRequest} from "../additionals/Requests";
+import GardenOptionContainer from "./garden_connected/GardenOptionContainer";
 
 function GardenPlanner() {
     // Refs
@@ -19,51 +13,6 @@ function GardenPlanner() {
     // States
     const [draggedVegetable, setDraggedVegetable] = useState({})
     const [selectedOptionList, setSelectedOptionList] = useState("Vegetables")
-    // Contexts
-    const [gardenSize, setGardenSize] = useContext(GardenSizeContext);
-    const [actualGardenId, setActualGardenId] = useContext(ActualGardenIdContext)
-
-    useEffect(() => {
-        if (!window.sessionStorage.getItem("token") || actualGardenId === null) return
-
-        const params = {garden_id: actualGardenId};
-        // Parameters: url, params, callbackSuccess, callbackError
-        getRequest('/api/get-garden-size', params, (response) => {
-            setGardenSize(prevData => ({
-                ...prevData,
-                rows: parseInt(response.data[0].row_count),
-                columns: parseInt(response.data[0].column_count),
-            }))
-        }, (error) => {
-            console.log(error.response.data)
-        })
-    }, [setGardenSize, actualGardenId])
-
-    const saveSizeChangesToDatabase = (rows, columns) => {
-        const data = {
-            garden_id: actualGardenId,
-            row_count: rows,
-            column_count: columns,
-        }
-        // Parameters: url, data, callbackSuccess, callbackError
-        putRequest('/api/update-garden-size', data, () => {}, () => {})
-    }
-
-    const modifyRows = () => {
-        setGardenSize(prevData => ({
-            ...prevData,
-            rows: gardenSize.rows + 1,
-        }))
-        saveSizeChangesToDatabase(gardenSize.rows + 1, gardenSize.columns)
-    }
-
-    const modifyColumns = () => {
-        setGardenSize(prevData => ({
-            ...prevData,
-            columns: gardenSize.columns + 1,
-        }))
-        saveSizeChangesToDatabase(gardenSize.rows, gardenSize.columns + 1)
-    }
 
     const renderSelectedOptionListComponent = () => {
         switch (selectedOptionList) {
@@ -76,50 +25,12 @@ function GardenPlanner() {
         }
     }
 
-    const addNewGarden = () => {
-        console.log("add new garden");
-    //     Axios to create new garden to DB
-        axios({
-            method: "post",
-            url: `${environmentVariables.BACKEND_URL}/api/add-new-garden`,
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: "application/json, text/plain, */*",
-                Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
-            },
-        }).then((res) => {
-            console.log(res.data);
-            setActualGardenId(res.data.id)
-        }).catch((error) => {
-            console.log(error.response.data)
-        })
-    }
-
     return (
         <div className="garden-planner">
             <div className="garden-container">
                 <GardenSelection/>
 
-                <div className="garden-option-container">
-
-                    <DownloadGarden gardenRef={gardenRef} rows={gardenSize.rows} columns={gardenSize.columns}/>
-
-                    <button className="option" onClick={() => modifyRows()}>
-                        <BsFillPlusCircleFill/>
-                        <span>Add row</span>
-                    </button>
-
-                    <button className="option" onClick={() => modifyColumns()}>
-                        <BsFillPlusCircleFill/>
-                        <span>Add column</span>
-                    </button>
-
-                    <button className="option" onClick={() => addNewGarden()}>
-                        <BsFillPlusCircleFill/>
-                        <span>New garden</span>
-                    </button>
-
-                </div>
+                <GardenOptionContainer gardenRef={gardenRef}/>
 
                 <Garden gardenRef={gardenRef}
                         draggedVegetable={draggedVegetable}
---
+++ frontend/src/component/garden_planner/garden_connected/GardenOptionContainer.js
@@ -0,0 +1,99 @@
+import React, {useContext, useEffect} from 'react';
+import DownloadGarden from "./garden_settings/DownloadGarden";
+import {BsFillPlusCircleFill} from "react-icons/bs";
+import {getRequest, putRequest} from "../../additionals/Requests";
+import {ActualGardenIdContext} from "./ActualGardenIdContext";
+import {GardenSizeContext} from "./garden_settings/GardenSizeContext";
+import axios from "axios";
+import {environmentVariables} from "../../../EnvironmentVariables";
+
+function GardenOptionContainer(props) {
+    const gardenRef = props.gardenRef;
+    const [actualGardenId, setActualGardenId] = useContext(ActualGardenIdContext)
+    const [gardenSize, setGardenSize] = useContext(GardenSizeContext);
+
+    useEffect(() => {
+        if (!window.sessionStorage.getItem("token") || actualGardenId === null) return
+
+        const params = {garden_id: actualGardenId};
+        // Parameters: url, params, callbackSuccess, callbackError
+        getRequest('/api/get-garden-size', params, (response) => {
+            setGardenSize(prevData => ({
+                ...prevData,
+                rows: parseInt(response.data[0].row_count),
+                columns: parseInt(response.data[0].column_count),
+            }))
+        }, (error) => {
+            console.log(error.response.data)
+        })
+    }, [setGardenSize, actualGardenId])
+
+    const saveSizeChangesToDatabase = (rows, columns) => {
+        const data = {
+            garden_id: actualGardenId,
+            row_count: rows,
+            column_count: columns,
+        }
+        // Parameters: url, data, callbackSuccess, callbackError
+        putRequest('/api/update-garden-size', data, () => {}, () => {})
+    }
+
+    const modifyRows = () => {
+        setGardenSize(prevData => ({
+            ...prevData,
+            rows: gardenSize.rows + 1,
+        }))
+        saveSizeChangesToDatabase(gardenSize.rows + 1, gardenSize.columns)
+    }
+
+    const modifyColumns = () => {
+        setGardenSize(prevData => ({
+            ...prevData,
+            columns: gardenSize.columns + 1,
+        }))
+        saveSizeChangesToDatabase(gardenSize.rows, gardenSize.columns + 1)
+    }
+
+    const addNewGarden = () => {
+        // Axios to create new garden to DB
+        axios({
+            method: "post",
+            url: `${environmentVariables.BACKEND_URL}/api/add-new-garden`,
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: "application/json, text/plain, */*",
+                Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
+            },
+        }).then((res) => {
+            console.log(res.data);
+            setActualGardenId(res.data.id)
+        }).catch((error) => {
+            console.log(error.response.data)
+        })
+    }
+
+    return (
+        <div className="garden-option-container">
+
+            <DownloadGarden gardenRef={gardenRef} rows={gardenSize.rows} columns={gardenSize.columns}/>
+
+            <button className="option" onClick={() => modifyRows()}>
+                <BsFillPlusCircleFill/>
+                <span>Add row</span>
+            </button>
+
+            <button className="option" onClick={() => modifyColumns()}>
+                <BsFillPlusCircleFill/>
+                <span>Add column</span>
+            </button>
+
+            <button className="option" onClick={() => addNewGarden()}>
+                <BsFillPlusCircleFill/>
+                <span>New garden</span>
+            </button>
+
+        </div>
+    );
+}
+
+export default GardenOptionContainer;
