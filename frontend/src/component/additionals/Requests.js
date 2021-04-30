90a5eed232b0263a2cfeb91d73defdde2474cd9a
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-04-30 16:44:55 +0200

    Refactor: Post and delete requests added.

--- frontend/src/component/additionals/Requests.js
+++ frontend/src/component/additionals/Requests.js
@@ -15,9 +15,37 @@ export const getRequest = (url, params, callbackSuccess, callbackError) => {
         .catch(callbackError)
 }
 
+export const postRequest = (url, data, callbackSuccess, callbackError) => {
+    axios({
+        method: "post",
+        url: `${environmentVariables.BACKEND_URL}${url}`,
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: "application/json, text/plain, */*",
+            Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
+        },
+        data: data,
+    }).then(callbackSuccess)
+        .catch(callbackError)
+}
+
 export const putRequest = (url, data, callbackSuccess, callbackError) => {
     axios({
         method: "put",
+        url: `${environmentVariables.BACKEND_URL}${url}`,
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: "application/json, text/plain, */*",
+            Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
+        },
+        data: data,
+    }).then(callbackSuccess)
+        .catch(callbackError)
+}
+
+export const deleteRequest = (url, data, callbackSuccess, callbackError) => {
+    axios({
+        method: "delete",
         url: `${environmentVariables.BACKEND_URL}${url}`,
         headers: {
             'Content-Type': 'application/json',
