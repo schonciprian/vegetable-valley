7de691b7605ddf27fcd8bbd4c01776beaaa589bd
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-05-07 17:01:10 +0200

    Refactor: PrivateRoute and PublicRoute component restructured and be able to accept Component or render method too and will handle it.

--- frontend/src/component/redirects/PublicRoute.js
+++ frontend/src/component/redirects/PublicRoute.js
@@ -2,17 +2,23 @@ import React, {useContext} from 'react'
 import { Redirect, Route } from 'react-router-dom'
 import {UserContext} from "../../context/User";
 
-const PublicRoute = ({ component: Component, ...rest }) => {
+const PublicRoute = ({ component: Component, render: render, ...rest }) => {
     const [user] = useContext(UserContext);
     const isLoggedIn = user.username;
+
+    if (isLoggedIn) {
+        return (<Route {...rest} render={props =>
+            <Redirect to={{pathname: '/', state: {from: props.location}}}/>
+        }/>)
+    }
 
     return (
         <Route
             {...rest}
             render={props =>
-                !isLoggedIn
-                    ? (<Component {...props} />)
-                    : (<Redirect to={{ pathname: '/', state: { from: props.location } }} />)
+                Component
+                    ? <Component {...props} />
+                    : render()
             }
         />
     )
