13fcf11e0a9aea756fe93eb825b06b1a2fd5ef48
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-05-07 17:03:00 +0200

    Refactor: LoadingContext default value changed to meet with the expectations of the context.

--- frontend/src/context/LoadingContext.js
+++ frontend/src/context/LoadingContext.js
@@ -1,6 +1,6 @@
 import React, { useState, createContext } from "react";
 
-export const LoadingContext = createContext(false);
+export const LoadingContext = createContext([false, () => {}]);
 
 export const LoadingProvider = (props) => {
     const [loading, setLoading] = useState(false);
