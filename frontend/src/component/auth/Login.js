80fb0797611434bb3cef2b8c8cc839692ec01686
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-05-18 12:08:29 +0200

    Refactor: Login component, left the usage of querySelectors.

--- frontend/src/component/auth/Login.js
+++ frontend/src/component/auth/Login.js
@@ -1,48 +1,63 @@
-import React, {useContext, useState} from 'react';
-import { useHistory } from "react-router-dom";
+import React, {useContext, useRef, useState} from 'react';
+import {useHistory} from "react-router-dom";
 import {UserContext} from "../../context/User";
 // Helpers
-import {removeError, handleShakingError} from "./AuthenticationHelper";
 import {postRequest} from "../additionals/Requests";
-import {authenticationFeedback} from "../additionals/SweetAlert";
+import {authenticationFeedbackPopUp, serviceUnavailablePopUp} from "../additionals/SweetAlert";
 // Stylesheets
 import '../../stylesheet/auth/Authentication.css';
 import '../../stylesheet/error/Error.css';
 //**************************************************//
 
 export default function Login() {
-    const [errorMessages, setErrorMessages] = useState({});
+    const [userData, setUserData] = useState({})
     const [, setUser] = useContext(UserContext);
+    const passwordRef = useRef()
+
+    const [errorMessages, setErrorMessages] = useState({});
+    const [shakingInputFields, setShakingInputFields] = useState([])
+
     const history = useHistory();
 
-    const getUserData = () => {
-        return {
-            email: document.getElementById("email").value,
-            password: document.getElementById("password").value,
-        }
-    }
-
     const loginRequest = async () => {
-        const userData = getUserData();
         postRequest('/api/login', userData,
             (response) => {
-            window.sessionStorage.setItem("token", response.data.token);
-            window.sessionStorage.setItem("username", response.data.username);
-            setUser({
-                "token": response.data.token,
-                "username": response.data.username,
-            })
-                authenticationFeedback("Successfully logged in", "Welcome back! You are redirected to the main page", "success", 2000, history, '/')
-        }, (error) => {
-            if (error.response === undefined) {
-                authenticationFeedback("Service unavailable", "Try again later", "error", 5000, history)
-                return;
-            }
+                window.sessionStorage.setItem("token", response.data.token);
+                window.sessionStorage.setItem("username", response.data.username);
+                setUser({
+                    "token": response.data.token,
+                    "username": response.data.username,
+                })
+                authenticationFeedbackPopUp("Successfully logged in", "Welcome back! You are redirected to the main page", "success", 2000, history, '/')
+            }, (error) => {
+                if (error.response === undefined) {
+                    serviceUnavailablePopUp("Service unavailable", "Try again later", 2000)
+                    return;
+                }
 
-            setErrorMessages(error.response.data);
-            if (Object.keys(error.response.data)[0] === 'failed') {return document.getElementById("password").value = ""}
-            Object.keys(error.response.data).forEach(error => {handleShakingError(error)})
-        })
+                setErrorMessages(error.response.data);
+                setShakingInputFields(Object.keys(error.response.data))
+                setTimeout(() => {
+                    setShakingInputFields([])
+                }, 1000)
+
+                if (Object.keys(error.response.data).includes('failed')) {
+                    removeUserData('password')
+                    passwordRef.current.value = "";
+                }
+            })
+    }
+
+    const removeErrorMessage = (type) => {
+        let errorMessagesCopy = {...errorMessages}
+        delete errorMessagesCopy[type]
+        setErrorMessages(errorMessagesCopy)
+    }
+
+    const removeUserData = (type) => {
+        let userDataCopy = {...userData}
+        delete userDataCopy[type]
+        setUserData(userDataCopy)
     }
 
     return (
@@ -50,28 +65,37 @@ export default function Login() {
             <h1>Login</h1>
             <form className="form">
                 {errorMessages.failed &&
-                    <div id='auth_failed' className="error-message error-message-authentication">
-                        {errorMessages.failed}
-                    </div>}
+                <div id='auth_failed' className="error-message error-message-authentication">
+                    {errorMessages.failed}
+                </div>}
 
                 <label htmlFor="email">Email:</label><br/>
                 {errorMessages.email && <div className="error-message">{errorMessages.email}</div>}
                 <input id="email"
-                       className={errorMessages.email ? "error" : ""}
+                       className={
+                           `${errorMessages.email ? 'error' : ""}` +
+                           `${shakingInputFields.includes('email') ? ' input-error' : ""}`
+                       }
                        type="email"
                        name="email"
                        placeholder="Enter your email"
-                       onClick={(event) => removeError(event)}
+                       onChange={(event) => setUserData(prevData => ({...prevData, email: event.target.value}))}
+                       onClick={(event) => removeErrorMessage('email')}
                        required/>
 
                 <label htmlFor="password">Password:</label><br/>
                 {errorMessages.password && <div className="error-message">{errorMessages.password}</div>}
                 <input id="password"
-                       className={errorMessages.password ? "error" : ""}
+                       className={
+                           `${errorMessages.password ? 'error' : ""}` +
+                           `${shakingInputFields.includes('password') ? ' input-error' : ""}`
+                       }
                        type="password"
                        name="password"
+                       ref={passwordRef}
                        placeholder="Enter your password"
-                       onClick={(event) => removeError(event)}
+                       onChange={(event) => setUserData(prevData => ({...prevData, password: event.target.value}))}
+                       onClick={(event) => removeErrorMessage('password')}
                        required/>
 
                 <button type="button" onClick={loginRequest}>Login</button>
